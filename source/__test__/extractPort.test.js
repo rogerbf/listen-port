4062794d48ef00b57c774e1bc71e88f98f53e16b
rogerbf <roger@bergfalk.nu>
2016-12-07 23:11:31 +0100

    working

---
+++ source/__test__/extractPort.test.js
@@ -0,0 +1,18 @@
+import extractPort from '../library/extractPort.js'
+
+test(`extractPort`, () => {
+  expect(extractPort).toBeDefined()
+
+  expect(extractPort()).toEqual(undefined)
+
+  expect(extractPort([])).toEqual(undefined)
+
+  expect(extractPort([{ NAME: `127.0.0.1:6258 (LISTEN)` }]))
+    .toEqual(6258)
+
+  expect(extractPort([
+    { NAME: `127.0.0.1:9050 (LISTEN)` },
+    { NAME: `127.0.0.1:9055 (LISTEN)` }
+  ]))
+    .toEqual([ 9050, 9055 ])
+})
---
+++ source/library/extractPort.js
@@ -0,0 +1,8 @@
+export default (filtered = []) => {
+  const results = filtered
+    .map(row => parseInt(row.NAME.match(/:\d*(?=\s)/)[0].slice(1)), 10)
+
+  return results.length > 1
+  ? results
+  : results[0]
+}
