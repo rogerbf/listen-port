54854b584fecbc0307515584ae05bc38f8e82d0f
rogerbf <roger@bergfalk.nu>
2016-12-07 23:29:02 +0100

    formatting

--- source/__test__/extractPort.test.js
+++ source/__test__/extractPort.test.js
@@ -7,12 +7,10 @@ test(`extractPort`, () => {
 
   expect(extractPort([])).toEqual(undefined)
 
-  expect(extractPort([{ NAME: `127.0.0.1:6258 (LISTEN)` }]))
-    .toEqual(6258)
+  expect(extractPort([{ NAME: `127.0.0.1:6258 (LISTEN)` }])).toEqual(6258)
 
   expect(extractPort([
     { NAME: `127.0.0.1:9050 (LISTEN)` },
     { NAME: `127.0.0.1:9055 (LISTEN)` }
-  ]))
-    .toEqual([ 9050, 9055 ])
+  ])).toEqual([ 9050, 9055 ])
 })
